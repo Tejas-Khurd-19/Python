d1739f4d95952e0d94c9f884b8dcc1466ce5ec66
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-01-25 20:57:36 +0530

    Add files via upload

---
+++ 7 Chapter Practice Set/02_problem2.py
@@ -0,0 +1,8 @@
+# White a program to greet all the person names sorted in a list l1 and starts wirh S
+# l₁ = ["Harry", "Stock", "Sachin", "Rahul"]
+
+l1 = ["Tejas", "Sanket", "Sachin", "Shyam"]
+
+for name in l1:
+    if(name.startswith("S")):
+        print(f"Hello {name}")
