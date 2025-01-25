8ad46693d4907e9e2a1d28febde0edf49f4e18de
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-01-25 16:03:17 +0530

    Add files via upload

---
+++ 4 Chapter Practice Set/03_problem3.py
@@ -0,0 +1,6 @@
+# Check wheather tuple can be changed in Python or not
+
+a = (34, 234, "Tejas")
+
+a[2] = "Khurd" # This will give following error
+# TypeError: 'tuple' object does not support item assignment
