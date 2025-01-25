40ed3ad124327aa3e5d528f2431224a045e8ee20
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-01-25 21:31:40 +0530

    Add files via upload

---
+++ 8 Chapter/01_functions.py
@@ -0,0 +1,28 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input "))("Enter your number:
+ 
+# average = (a + b + c)/3
+# print(average)
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+ 
+# average = (a + b + c)/3
+# print(average)
+
+# Instead of writing separate we will collect it into functions.
+
+# Function Definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+    
+    average = (a + b + c)/3
+    print(average)
+
+
+avg() # Function Call
+print("Thank you!")
---
+++ 8 Chapter/02_quick_quiz.py
@@ -0,0 +1,9 @@
+# Write a program to greet user 'Good Day' using function
+def goodDay(name):
+    # print("Good Day " + name)
+    print("Good Day ". name)
+
+def main():
+    name = input("Enter your name")
+    goodDay(name)
+    
