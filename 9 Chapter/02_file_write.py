3bc0ae0234fd948bf692fc162179fa4ea2c6f8f8
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-01-27 23:12:12 +0530

    Add files via upload

---
+++ 9 Chapter/02_file_write.py
@@ -0,0 +1,7 @@
+str = ", Hey, Python is amazing"
+
+f = open("myfile.txt", "w")
+
+f.write(str)
+
+f.close()
---
+++ 9 Chapter/04_append.py
@@ -0,0 +1,7 @@
+str = ", Hey Tejas you are amazing"
+
+f = open("myfile.txt", "a")
+
+f.write(str)
+
+f.close()
