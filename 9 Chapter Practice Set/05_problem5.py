ac49d9faa9291c30450fe5899da11379e18fef36
Tejas Sandeep Khurd <tejkhurd09@gmail.com>
2025-01-27 23:39:40 +0530

    Add files via upload

---
+++ 9 Chapter Practice Set/05_problem5.py
@@ -0,0 +1,14 @@
+# Repeat program 4 for a list of such words to be censored.
+
+# I have not use this code you can use is to verify
+
+words = ["Donkey", "bad", "ganda"]
+
+with open("file.txt", "r") as f:
+    content = f.read()
+
+for word in words:
+    content = content.replace(word, "#" * len(word))
+
+with open("file.txt", "w") as f:
+    f.write(content)
---
+++ 9 Chapter Practice Set/10_problem10.py
@@ -0,0 +1,4 @@
+# Write a program to wipe out the content of a file using python.
+
+with open("this_copy.txt", "w") as f:
+    f.write("") # you can use to wipe this_copy.txt, I have not use.
